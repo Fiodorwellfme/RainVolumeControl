75fdc82783e39e50181694a186bf1fe15bb32904
Theodore <39596407+Fiodorwellfme@users.noreply.github.com>
2026-04-03 17:56:16 +0200

    Update Settings.cs
    Added the relevant setting for BTR volume control.

--- Settings.cs
+++ Settings.cs
@@ -5,7 +5,8 @@ namespace RainVolumeControl
     public static class Settings
     {
         public static ConfigEntry<float> RainVolumeMultiplier { get; private set; }
-
+        public static ConfigEntry<float> BTRVolumeMultiplier { get; private set; }
+        
         public static void Init(ConfigFile config)
         {
             RainVolumeMultiplier = config.Bind(
@@ -14,6 +15,15 @@ namespace RainVolumeControl
                 1f,
                 new ConfigDescription(
                     "Multiplier applied to the rain volume (1 is same as vanilla)",
+                    new AcceptableValueRange<float>(0f, 2f)
+                )
+            );
+            BTRVolumeMultiplier = config.Bind(
+                "General",
+                "BTR Volume Multiplier",
+                1f,
+                new ConfigDescription(
+                    "Multiplier applied to BTR movement transition volume (1 is same as vanilla)",
                     new AcceptableValueRange<float>(0f, 2f)
                 )
             );
