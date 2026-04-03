9b343af3ec9982a0739ae18f0f083fddb3306ab4
Theodore <39596407+Fiodorwellfme@users.noreply.github.com>
2026-04-03 17:47:22 +0200

    Update BTRVolumeMultiplierPatch.cs

--- Patches/BTRVolumeMultiplierPatch.cs
+++ Patches/BTRVolumeMultiplierPatch.cs
@@ -1,1 +1,27 @@
+﻿using Audio.Vehicles.BTR;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
 
+namespace RainVolumeControl.Patches
+{
+    internal class BTRVolumeMultiplierPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(BtrSoundController), "method_10");
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(GInterface218 inControl, GInterface218 outControl, EnvironmentType environment, float t)
+        {
+            float multiplier = Settings.BTRVolumeMultiplier.Value;    
+            GInterface218 active = (environment == EnvironmentType.Outdoor) ? outControl : inControl;
+            GInterface218 inactive = (environment == EnvironmentType.Outdoor) ? inControl : outControl;     
+            inactive.SetBaseVolume((1f - t) * multiplier);
+            active.SetBaseVolume(t * multiplier);
+        
+            return false;
+        }
+    }
+}
