017dbd7f34001f2a994141b5016da915fabdee36
Theodore <39596407+Fiodorwellfme@users.noreply.github.com>
2026-04-05 20:11:44 +0200

    Update AirdropVolumeMultiplierPatch.cs
    Added Airplane patch because I forgot

--- Patches/AirdropVolumeMultiplierPatch.cs
+++ Patches/AirdropVolumeMultiplierPatch.cs
@@ -37,4 +37,37 @@ namespace RainVolumeControl.Patches
             return false;
         }
     }
+    
+    public class AirplaneVolumePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(AirplaneLogicClass), "method_1", new[] { typeof(TaggedClip)});
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(AirplaneLogicClass __instance, TaggedClip clip)
+        {
+            float multiplier = Settings.AirdropVolumeMultiplier.Value;
+            
+    		float num = __instance.method_3();
+    		__instance.BetterSource_0.SetBaseVolume(EFTHardSettings.Instance.PlaneVolumeCurve.Evaluate(num) * multiplier);
+    		__instance.BetterSource_0.SetPitch(Mathf.Lerp(0.7f, 1.1f, num));
+    		__instance.BetterSource_0.source1.spread = Mathf.Lerp(60f, 180f, num);
+    		if (__instance.BetterSource_0.PlayBackState == BetterSource.EPlayBackState.Playing)
+    		{
+    			return false;
+    		}
+    		__instance.BetterSource_0.source1.rolloffMode = AudioRolloffMode.Custom;
+    		__instance.BetterSource_0.source1.SetCustomCurve(AudioSourceCurveType.CustomRolloff, EFTHardSettings.Instance.SoundRolloff);
+    		__instance.BetterSource_0.source1.clip = clip.Clip;
+    		__instance.BetterSource_0.Loop = true;
+    		__instance.BetterSource_0.source1.dopplerLevel = 1.5f;
+    		__instance.BetterSource_0.SetSpatialBlend(1f);
+    		__instance.BetterSource_0.SetRolloff((float)clip.Falloff);
+    		__instance.BetterSource_0.Play(clip.Clip, null, 1f, 1f, false, false);
+            
+            return false;
+        }
+    }
 }
